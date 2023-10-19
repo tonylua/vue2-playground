cc9e627e125c3a842549aacc91aa5c50c92611f4
tony lua <tonylua@tonydeMacBook-Pro.local>
2023-10-19 20:25:56 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,71 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <codemirror v-model="code" :options="cmOptions" @ready="onCmReady" />
+    <component :is="remote" v-if="remote" v-bind="$attrs" v-on="$listeners" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Vue from "vue/dist/vue.common";
+import dayjs from "dayjs";
+import { loadModule } from "vue3-sfc-loader/dist/vue2-sfc-loader";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  components: {},
+  data() {
+    return {
+      remote: null,
+      // TODO
+      code: `<template functional>
+  <div class="sample">
+    hello excel!
+  </div>
+</template>
+
+<style>
+  .sample {
+    color: red;
   }
-}
+</style>
+      `,
+      cmOptions: {},
+    };
+  },
+  methods: {
+    onCmReady(cm) {
+      cm.setSize(600, 360);
+    },
+  },
+  mounted() {
+    loadModule("/vue-sample/src/components/HelloWorld.vue", {
+      moduleCache: {
+        vue: Vue,
+        dayjs,
+      },
+      async getFile(url) {
+        const res = await fetch(url);
+        if (!res.ok) {
+          throw Object.assign(new Error(`${res.statusText}  ${url}`), { res });
+        }
+        return {
+          getContentData: (asBinary) =>
+            asBinary ? res.arrayBuffer() : res.text(),
+        };
+      },
+      addStyle(textContent) {
+        const style = Object.assign(document.createElement("style"), {
+          textContent,
+        });
+        const ref = document.head.getElementsByTagName("style")[0] || null;
+        document.head.insertBefore(style, ref);
+      },
+    }).then((comp) => {
+      console.log(comp, "remote sfc loaded");
+      this.remote = comp;
+    });
+  },
+};
 </script>
 
 <style>
@@ -21,8 +73,11 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
   margin-top: 60px;
+  display: flex;
+  flex-direction: row;
+  gap: 20px;
 }
 </style>
